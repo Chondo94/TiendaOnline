8c10c227024d0336f26f50fe2b574c77f83e3abc
Chondo94 <estuardobojorquezcortez@gmail.com>
2020-07-03 20:21:26 -0600

    Se creo la vista y el controlador para registrar usuarios

--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -4,4 +4,16 @@ class usuarioController{
     public function index(){
         echo "Controlador Usuarios, Accion Index";
     }
+
+    //Metodo para Registrar usuarios
+    public function registro(){
+        require_once 'views/usuario/registro.php';
+    }
+
+    // Metodo para Guardar el usuario
+    public function save(){
+        if(isset($_POST)){
+            var_dump($_POST);
+        }
+    }
 }
